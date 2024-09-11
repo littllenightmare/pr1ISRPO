6b38cbdc740c4d7f4c33cad254bb5dd8a3d721a7
исп-41 <-41@LAB21-07>
2024-09-11 10:46:03 +0300

    Добавьте файлы проекта.

---
+++ pr1ISRPO/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr1ISRPO
+{
+    internal class Class1
+    {
+        public double[] func(double[,] matr)
+        {
+            int str = matr.GetLength(0);
+            int stolb = matr.GetLength(1);
+            double pr = 1, sum=0, min=10000, max=-10000;
+            double[]b = new double[str*4];
+
+
+            for (int i = 0; i < str; i++)
+            {
+                for (int j = 0; j < stolb; j++)
+                {
+                    pr=pr*matr[i,j];
+                }
+                b[i] = pr;
+            }
+
+
+            for (int i = 0; i < stolb; i++)
+            {
+                for (int j = 0; j < str; j++)
+                {
+                    sum = sum+matr[i,j];
+                }
+                b[str+i] = sum/str;
+            }
+
+
+            for (int i = 0; i < stolb; i++)
+            {
+                for (int j = 0; j < str; j++)
+                {
+                    if (matr[i, j] < 0)
+                    {
+                        b[str+str + i] = matr[i,j];
+                    }
+                }
+            }
+
+
+            for (int i = 0; i < str; i++)
+            {
+                for (int j = 0; j < stolb; j++)
+                {
+                    if (matr[i, j] < min)
+                    {
+                        min = matr[i, j];
+                    }
+                    if (matr[i, j] > max)
+                    {
+                        max=matr[i, j];
+                    }
+                }
+                b[str+str+str + i] = max - min;
+            }
+
+            return b;
+        }
+    }
+}
---
+++ pr1ISRPO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+Console.WriteLine("Введите матрицу (числа в строке писать через запятую, при переходе на новую строку поставить точку с запятой, пробелы между числами не ставить)");
+string s = Console.ReadLine();
+try
+{
+    for (int i = 0; i < s.Length; i++)
+    {
+        double ch = Char.GetNumericValue(s, i);
+        if (s[i] == ',')
+        {
+
+        }
+        if (s[i] == ';')
+        {
+
+        }
+    }
+}
+catch (Exception)
+{
+    Console.WriteLine("Ну я же говорила, как написать матрицу");
+}
